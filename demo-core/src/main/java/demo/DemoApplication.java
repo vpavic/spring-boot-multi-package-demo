3acb5a5d4f393bed239c8a570b8ace24194deef6
Vedran Pavic <vedran.pavic@gmail.com>
2017-07-03 08:57:51 +0200

    Downgrade Spring Boot to 1.5.4.RELEASE

--- demo-core/src/main/java/demo/DemoApplication.java
+++ demo-core/src/main/java/demo/DemoApplication.java
@@ -3,10 +3,10 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class DemoApplication implements WebMvcConfigurer {
+public class DemoApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
