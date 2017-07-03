fdb8282c685d08ce1e23b77fbc0cc2f7488f5a09
Vedran Pavic <vedran.pavic@gmail.com>
2017-07-03 09:45:57 +0200

    Fix missing servlet initializer

--- demo-core/src/main/java/demo/DemoApplication.java
+++ demo-core/src/main/java/demo/DemoApplication.java
@@ -2,19 +2,32 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class DemoApplication extends WebMvcConfigurerAdapter {
+public class DemoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DemoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("home");
+	@Configuration
+	static class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addViewController("/").setViewName("home");
+		}
+
 	}
 
 }
